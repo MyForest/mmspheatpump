7d1f9e801d5228828046a654669e88a8c3a123a3
David Bowen <david@myforest.com>
2020-03-05 00:43:52 +0000

    A way to completely break everything for testing

--- Lib/application-configuration.js
+++ Lib/application-configuration.js
@@ -3,6 +3,9 @@ async function configureApplicationOnClientSide() {
     // Fetch shape of application from the server
     config.app = await (await fetch(applicationURL + "config/application-configuration.json")).json()
 
+    // Break all the feeds for testing
+    // config.app = {}
+
     // Load the available feeds for this user's system so we can configure the application
     config.feeds = await (await fetch("../feed/list.json")).json()
 
