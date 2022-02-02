18732880970b35f6f2cbce6707e4a53b1966b5dc
David Bowen <david@myforest.com>
2022-02-02 19:47:42 +0000

    Fix comments around multi-version piece of code
    Thanks to mjr for spotting it:
    
    https://community.openenergymonitor.org/t/heat-pump-experimentation-app-release-news/13423/14?u=myforest

--- mmspheatpump.php
+++ mmspheatpump.php
@@ -13,9 +13,9 @@
 <script type="text/javascript" src="<?php echo $path; ?>Lib/flot/jquery.flot.resize.min.js?v=<?php echo $v; ?>"></script>
 <script type="text/javascript" src="<?php echo $path; ?>Lib/flot/jquery.flot.time.min.js?v=<?php echo $v; ?>"></script>
 <script type="text/javascript" src="<?php echo $path; ?>Lib/flot/jquery.flot.selection.min.js?v=<?php echo $v; ?>"></script>
-# EmonCMS 10.x
+<!-- EmonCMS 10.x -->
 <script type="text/javascript" src="<?php echo $path; ?>Modules/app/Lib/vis.helper.js?v=<?php echo $v; ?>"></script>
-# EmonCMS 11.x
+<!-- EmonCMS 11.x -->
 <script type="text/javascript" src="<?php echo $path; ?>Lib/vis.helper.js?v=<?php echo $v; ?>"></script>
 
 
